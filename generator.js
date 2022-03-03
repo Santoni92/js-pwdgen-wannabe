3937b073e3aa10f60146a59faa94ee1653f8b5cf
Santoni92 <98034624+Santoni92@users.noreply.github.com>
2022-03-03 16:31:34 +0100

    aggiunta scritta sul  (oggetto?) document per dire all'utente che quella in output è la sua password

--- generator.js
+++ generator.js
@@ -1,21 +1,27 @@
 console.log('JS Ok');
 
 //costante che serve per memorizzare il nome richiesto all'utente tramite prompt() (usare la convenzione camel case per i nomi composti)
-const nomeUtente; //dichiaro la costante
-
-nomeUtente = prompt('Inserisci il tuo nome:');   //assegno il valore alla costante chiedendo all'utente di inserire il valore
+const nomeUtente = prompt('Inserisci il tuo nome:');   /* dichiaro ed assegno il valore alla costante chiedendo all'utente di inserire il valore;
+                                                           per una costante non è possibile separare la fase di dichiarazione da quella di inizializzazione*/
+                                                          
 
 //attenzione che prompt() restituisce,indipendentemente dal tipo di dato inserito, sempre un valore di tipo stringa
 
+
+
 //costante che serve per memorizzare il cognome richiesto all'utente tramite prompt() (usare la convenzione camel case per i nomi composti)
-const cognomeUtente;    //dischiaro la costante
+const cognomeUtente = prompt('Inserisci il tuo cognome:');
 
-cognomeUtente = prompt('Inserisci il tuo cognome:');
+//costante che serve per memorizzare il colore preferito richiesto all'utente tramite prompt() (usare la convenzione camel case per i nomi composti)
 
-const colorePreferito;
+const colorePreferito  = prompt('Inserisci il nome del tuo colore preferito:');
 
-const colorePreferito = prompt('Inserisci il nome del tuo colore preferito:');
+document.writeln('Password generata per l\'utente:');   //notare il carattere di escape \' per poter inserire il singolo apice così da scrivere l'apostrofo
+document.writeln(nomeUtente + cognomeUtente + colorePreferito + 21); //istruziopne che serve per stampare sul documento html
+                                                                     /*il numero 21 è trattato come una stringa in quanto
+                                                                      nell'argomento di writeln vi sono stringhe e quindi
+                                                                       il numero è automaticamente trattato come stringa*/
 
-document.writeln(nomeUtente + cognomeUtente + colorePreferito);
+                                                                       
 
 
